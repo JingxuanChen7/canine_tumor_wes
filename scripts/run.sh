63e154f7259a7c3bc629d15d730040341b3b2b4e
Jingxuan Chen <jc33471@uga.edu>
2023-09-08 16:43:06 -0400

    update scripts for submission

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --partition=iob_p
-#SBATCH --job-name=wes_mapping
+#SBATCH --job-name=test
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=8
 #SBATCH --mem=60G
 #SBATCH --time=100:00:00
 #SBATCH --mail-user=jc33471@uga.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --output=/scratch/jc33471/canine_tumor/CMT-100.out
+#SBATCH --output=/scratch/jc33471/canine_tumor/test.out
 
 # install conda env
 # mamba env create --force -f /home/jc33471/canine_tumor_wes/scripts/envs/wes_env.yml --name wes_env
@@ -28,22 +28,21 @@ mkdir -p ${run_dir}
 cd ${run_dir}
 mkdir -p logs/ config/ data/ out/
 
-config="/home/jc33471/canine_tumor_wes/scripts/per_case/config.json"
-# config=$run_dir/config/test.json
+# config="/home/jc33471/canine_tumor_wes/scripts/per_case/config.json"
+config=$run_dir/config/test.json
 
 python ${project_dir}/scripts/per_case/make_snakemake_config.py \
     --project_dir ${project_dir} \
-    --out ${run_dir}/config/test.json \
+    --out ${config} \
     --outdir ${run_dir} \
-    --Bioproject "PRJNA489159" \
-    --Normal_Run "SRR7780976" \
-    --Tumor_Run "SRR7780979" \
-    --CaseName "CMT-100" \
+    --Bioproject "PRJNA677995" \
+    --Normal_Run "SRR13050752" \
+    --Tumor_Run "SRR13050748-SRR13050750-SRR13050751" \
+    --CaseName "Pt03" \
     --threads 8 \
     --memory "60G"
 
 snakemake \
-    --dry-run \
     --cores ${SLURM_NTASKS} \
     --use-conda \
     --configfile ${config} \
@@ -57,3 +56,17 @@ snakemake \
 #     --configfile ${config} \
 #     --snakefile "${project_dir}/scripts/per_case/Snakefile" | dot -Tpdf > dag.pdf
 
+## validate 10 samples
+for case in "CMT-100" "CMT-102" "CMT-103" "CMT-105" "CMT-106" "CMT-107" "CMT-109" "CMT-111" "CMT-112" "CMT-114"; do
+    wc -l /project/szlab/Kun_Lin/Pan_Cancer/Mammary_Cancer/Germline/${case}/*_rg_added_sorted_dedupped_removed.realigned.bam.filter.vcf-PASS-avinput.exonic_variant_function_WithGeneName
+    wc -l /scratch/jc33471/canine_tumor/results/Germline/PRJNA489159/${case}/*_rg_added_sorted_dedupped_removed.realigned.bam.filter.vcf-PASS-avinput.exonic_variant_function_WithGeneName
+
+done 
+
+for case in "CMT-100" "CMT-102" "CMT-103" "CMT-105" "CMT-106" "CMT-107" "CMT-109" "CMT-111" "CMT-112" "CMT-114"; do
+    wc -l /project/szlab/Kun_Lin/Pan_Cancer/Mammary_Cancer/DepthOfCoverage/${case}/*_rg_added_sorted_dedupped_removed.realigned.bam.filter.vcf-PASS-avinput.exonic_variant_function_WithGeneName
+    wc -l /scratch/jc33471/canine_tumor/results/DepthOfCoverage/PRJNA489159/${case}/*_rg_added_sorted_dedupped_removed.realigned.bam.filter.vcf-PASS-avinput.exonic_variant_function_WithGeneName
+
+done
+diff -i -E -Z -w -y /project/szlab/Kun_Lin/Pan_Cancer/Mammary_Cancer/Germline/CMT-100/SRR7780976_rg_added_sorted_dedupped_removed.realigned.bam.filter.vcf-PASS-avinput.exonic_variant_function_WithGeneName \
+    /scratch/jc33471/canine_tumor/results/Germline/PRJNA489159/CMT-100/SRR7780976_rg_added_sorted_dedupped_removed.realigned.bam.filter.vcf-PASS-avinput.exonic_variant_function_WithGeneName
--- scripts/sum_cases/run.sh
+++ scripts/sum_cases/run.sh
@@ -2,22 +2,23 @@
 #SBATCH --partition=iob_p
 #SBATCH --job-name=master_job
 #SBATCH --nodes=1
+#SBATCH --ntasks=1
 #SBATCH --tasks-per-node=8
 #SBATCH --mem=60G
 #SBATCH --time=500:00:00
 #SBATCH --mail-user=jc33471@uga.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --output=/scratch/jc33471/canine_tumor/master_job.out
+#SBATCH --output=/scratch/jc33471/canine_tumor_0908/master_job.out
 
 CONDA_BASE=$(conda info --base)
 source ${CONDA_BASE}/etc/profile.d/conda.sh
 conda activate wes_env
 
 project_dir="/home/${USER}/canine_tumor_wes"
-run_dir="/scratch/${USER}/canine_tumor"
+run_dir="/scratch/${USER}/canine_tumor_0908"
 mkdir -p ${run_dir}
 cd ${run_dir}
-mkdir -p logs/ config/ data/ out/ results
+mkdir -p logs/ config/ data/ out/ results/
 
 # config="/home/jc33471/canine_tumor_wes/scripts/sum_cases/config.json"
 config=$run_dir/config/master_config.json
@@ -31,7 +32,6 @@ python ${project_dir}/scripts/sum_cases/make_snakemake_config.py \
     --memory "60G"
 
 snakemake \
-    --dry-run \
     --jobs 200 \
     --use-conda \
     --latency-wait 60 \
@@ -46,6 +46,7 @@ snakemake \
             --partition=batch \
             --job-name={wildcards.Bioproject}_{wildcards.CaseName}_smk \
             --nodes=1 \
+            --ntasks=1 \
             --tasks-per-node={threads} \
             --mem={resources.mem} \
             --time=72:00:00 \
