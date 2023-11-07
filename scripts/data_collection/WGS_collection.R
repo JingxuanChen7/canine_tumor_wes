2f7a43b13fe5e06336de8191e3a60c854f640a30
Jingxuan Chen <44645406+JingxuanChen7@users.noreply.github.com>
2023-11-07 09:25:04 -0500

    Update WGS_collection.R

--- scripts/data_collection/WGS_collection.R
+++ scripts/data_collection/WGS_collection.R
@@ -10,15 +10,21 @@ dog10K <- read_excel(paste0(meta_dir, "/WGS/Dog10K_supp_table.xlsx"), skip = 2) 
   select(c("Sample Name","Effective Autosomal Mean Coverage","Breed/Type")) %>%
   rename("Case_ID" = "Sample Name", "Coverage" = "Effective Autosomal Mean Coverage", "Breed" = "Breed/Type")
 
+
+
 nih <- read_excel(paste0(meta_dir, "/WGS/NIH_supp.xlsx"), skip = 1) %>%
-  filter(BioProject == "PRJNA448733") %>%
+  #filter(BioProject == "PRJNA448733") %>%
   select(c("Name_ID_SRA","CoverageAll","Phylo_Results")) %>%
   rename("Case_ID" = "Name_ID_SRA", "Coverage" = "CoverageAll", "Breed" = "Phylo_Results")
 
 dbvdc <- read_excel(paste0(meta_dir, "/WGS/DBVDC_supp.xlsx"), skip = 1) %>%
-  filter(`Study accession` == "PRJEB16012") %>%
+  #filter(`Study accession` == "PRJEB16012") %>%
   select(c("Sample ID","Coverage depth","Breed")) %>%
   rename("Case_ID" = "Sample ID", "Coverage" = "Coverage depth")
 
 all_wgs <- rbind(dog10K,nih,dbvdc)
 hist(all_wgs$Coverage, breaks = 50)
+
+# breed frequency
+dog10K_freq <- table(dog10K$Breed)[order(table(dog10K$Breed), decreasing = T)]
+nih_freq <- table(nih$Breed)[order(table(nih$Breed), decreasing = T)]
